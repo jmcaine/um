218d4646a0ac7f3004362e70b9949d81b40f62df
jmcaine <jmcaine@caines.us>
2025-08-18 19:15:46 -0700

    added periodic_emailer support

---
+++ app/periodic_emailer.py
@@ -0,0 +1,62 @@
+
+import sqlite3
+
+from . import emailer
+
+
+dbc = sqlite3.connect('um.db', isolation_level = None)
+dbc.row_factory = sqlite3.Row
+
+#f'select tag.name, count(*) from message join message_tag on message_tag.message = message.id join tag on message_tag.tag = tag.id join user_tag on user_tag.tag = tag.id join user_tag.user = ? and user_tag.tag = tag.id where not exists (select 1 from message_stashed where message.id = message_stashed.message and user.id = message_stashed.stashed_by)'
+
+unstashed_count_sql = '''
+select tag.name, count(message.id) as count
+    from message
+    join message_tag on message_tag.message = message.id
+    join tag on message_tag.tag = tag.id
+    join user_tag on tag.id = user_tag.tag
+    join user on user_tag.user = user.id
+    where not exists (select 1 from message_stashed where message.id = message_stashed.message and user.id = message_stashed.stashed_by)
+    and user.id = ? and message.deleted is null
+''' # group by tag.name (unnecessary, implied)
+
+unstashed_by_others_sql = '''
+	select message.teaser, group_concat(user.username, ', ') as users
+	from message
+	join message_tag on message_tag.message = message.id
+	join tag on message_tag.tag = tag.id
+	join user_tag on tag.id = user_tag.tag
+	join user on user_tag.user = user.id
+	where not exists (select 1 from message_stashed where message.id = message_stashed.message and user.id = message_stashed.stashed_by)
+	and message.author = ? and message.deleted is null
+	group by message.id
+'''
+
+users = '''
+	select user.id, person.first_name, person.last_name, email.email
+	from user
+	join person on user.person = person.id
+	join email on email.person = person.id
+	where user.active = 1
+	and user.id = 1
+'''
+
+def run():
+	for u in dbc.execute(users, ()):
+		print(f"{u['first_name']} {u['last_name']}")
+		unstashed_counts = dbc.execute(unstashed_count_sql, (u['id'],))
+		unstashed_count_lines = [f"{count['name']} - {count['count']} unstashed messages" for count in unstashed_counts] if unstashed_counts else []
+		unstashed_total_count = sum([count['count'] for count in unstashed_counts]) if unstashed_counts else 0
+		unstashed_by_others = dbc.execute(unstashed_by_others_sql, (u['id'],))
+		unstashed_by_others_lines = [f"""{unstashed['users']} have not yet read your message: "{unstashed['teaser']}".""" for unstashed in unstashed_by_others] if unstashed_by_others else []
+		paragraphs = [
+			f"{u['first_name']} {u['last_name']},",
+			f"You currently have {unstashed_total_count} unstashed messages." + ("  Go to https://um.openhome.school/ to stash some messages!" if unstashed_total_count else ""),
+		] + unstashed_count_lines + unstashed_by_others_lines
+		text = '\n\n'.join(paragraphs)
+		html = '<html><body>' + ''.join(['<p>' + paragraph + '</p>' for paragraph in paragraphs]) + '</body></html>'
+
+		emailer.send_email(u['email'], '"Um..." digest', text, html)
+
+if __name__ == '__main__':
+	run()
