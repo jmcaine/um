0dcb3c145015b9215f46e7a171af0537a767e55b
jmcaine <jmcaine@caines.us>
2025-09-16 00:44:58 -0700

    added

---
+++ static/js/assignments.js
@@ -0,0 +1,28 @@
+
+// TODO: i18n; see equivalent text. strings
+const t_loading_assignments = 'Loading assignments...';
+
+
+let assignments = {
+
+	send_ws: function(task, fields) {
+		ws_send_task('app.assignments', task, fields);
+	},
+
+	// ---------------
+
+	filter: function(id, filt) {
+		assignments.send_ws('main', {filt: filt});
+		$('assignments_container').innerHTML = t_loading_assignments; // set placeholder, awaiting load...
+	},
+
+	show_assignments: function(content) {
+		hide_dialog();
+		set_sub_content('assignments_container', content);
+	},
+
+	mark_complete: function(assignment_id, checkbox) {
+		assignments.send_ws('mark_complete', {assignment_id: assignment_id, checked: (checkbox.checked == true)});
+	},
+
+};
